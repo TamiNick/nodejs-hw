b1839b6c43abc0ec7ecede5ad6d8e5e5638b73e2
Dmytro <dmytriy.litvin@gmail.com>
2026-03-18 20:22:43 +0200

    done

---
+++ src/controllers/userController.js
@@ -0,0 +1,24 @@
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+
+export const updateUserAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      throw createHttpError(400, 'No file');
+    }
+
+    const photo = await saveFileToCloudinary(req.file.buffer);
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: photo.secure_url },
+      { new: true }
+    );
+
+    res.json({
+      url: user.avatar,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
